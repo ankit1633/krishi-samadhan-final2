e47f91cda2e4271b1b1df849f9f33552ec66e853
ankit1633 <111997421+ankit1633@users.noreply.github.com>
2024-08-31 20:49:15 +0530

    finally5

--- server/frontend/src/service/api.js
+++ server/frontend/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = process.env.REACT_APP_API_URL || '';;
+const URL = `${window.location.origin}`;
 
 export const authenticateSignup = async (data) => {
     try {
