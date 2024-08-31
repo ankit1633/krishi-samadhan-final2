3b1fdcabc7e0ea715e6b358afb9a36e8536f35cf
ankit1633 <111997421+ankit1633@users.noreply.github.com>
2024-08-31 20:54:38 +0530

    finally7

--- server/index.js
+++ server/index.js
@@ -21,11 +21,7 @@ const __dirname = dirname(__filename);
 const app = express();
 
 // Middleware
-const corsOptions = {
-  origin: ['https://krishi-samadhan-final2-unkt.vercel.app/'], // Replace with your Vercel front-end URL
-  credentials: true,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser()); // Added cookie-parser
